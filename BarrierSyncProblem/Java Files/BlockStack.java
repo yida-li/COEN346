08b01637d7a6dd9a3c0438f629f8771438f82183
Yida Li <gabrielyidali@example.com>
2020-11-01 14:53:43 -0500

    added isEmpty method()

--- BarrierSyncProblem/Java Files/BlockStack.java
+++ BarrierSyncProblem/Java Files/BlockStack.java
@@ -1,16 +1,13 @@
 /**
- * Class BlockStack
- * Implements character block stack and operations upon it.
+ * Class BlockStack Implements character block stack and operations upon it.
  *
- * $Revision: 1.4 $
- * $Last Revision Date: 2019/07/02 $
+ * $Revision: 1.4 $ $Last Revision Date: 2019/07/02 $
  *
- * @author Serguei A. Mokhov, mokhov@cs.concordia.ca;
- * Inspired by an earlier code by Prof. D. Probst
-
+ * @author Serguei A. Mokhov, mokhov@cs.concordia.ca; Inspired by an earlier
+ *         code by Prof. D. Probst
+ * 
  */
-class BlockStack
-{
+class BlockStack {
 	/**
 	 * # of letters in the English alphabet + 2
 	 */
@@ -29,79 +26,84 @@ class BlockStack
 	/**
 	 * Current top of the stack
 	 */
-	public int iTop  = 3;
+	public int iTop = 3;
 
 	/**
 	 * stack[0:5] with four defined values
 	 */
-	public char acStack[] = new char[] {'a', 'b', 'c', 'd', '$', '$'};
+	public char acStack[] = new char[] { 'a', 'b', 'c', 'd', '$', '$' };
 
 	/**
 	 * Default constructor
 	 */
-	public BlockStack()
-	{
+	public BlockStack() {
 	}
 
 	/**
 	 * Supplied size
 	 */
-	public BlockStack(final int piSize)
-	{
+	public BlockStack(final int piSize) {
 
-
-                if(piSize != DEFAULT_SIZE)
-		{
+		if (piSize != DEFAULT_SIZE) {
 			this.acStack = new char[piSize];
 
 			// Fill in with letters of the alphabet and keep
 			// 2 free blocks
-			for(int i = 0; i < piSize - 2; i++)
-				this.acStack[i] = (char)('a' + i);
+			for (int i = 0; i < piSize - 2; i++)
+				this.acStack[i] = (char) ('a' + i);
 
 			this.acStack[piSize - 2] = this.acStack[piSize - 1] = '$';
 
 			this.iTop = piSize - 3;
-                        this.iSize = piSize;
+			this.iSize = piSize;
 		}
 	}
 
 	/**
 	 * Picks a value from the top without modifying the stack
+	 * 
 	 * @return top element of the stack, char
 	 */
-	public char pick()
-	{
+	public char pick() {
 		return this.acStack[this.iTop];
 	}
 
 	/**
 	 * Returns arbitrary value from the stack array
+	 * 
 	 * @return the element, char
 	 */
-	public char getAt(final int piPosition)
-	{
+	public char getAt(final int piPosition) {
 		return this.acStack[piPosition];
 	}
 
 	/**
 	 * Standard push operation
 	 */
-	public void push(final char pcBlock)
-	{
+	public void push(final char pcBlock) {
 		this.acStack[++this.iTop] = pcBlock;
 	}
 
 	/**
 	 * Standard pop operation
+	 * 
 	 * @return ex-top element of the stack, char
 	 */
-	public char pop()
-	{
+	public char pop() {
 		char cBlock = this.acStack[this.iTop];
 		this.acStack[this.iTop--] = '$'; // Leave prev. value undefined
 		return cBlock;
 	}
+
+	/**
+	 * Standard check operation
+	 * 
+	 * @return true if stack is empty
+	 */
+	public boolean isEmpty() {
+		return (this.iTop == -1);
+	}
+
 }
 
 // EOF
